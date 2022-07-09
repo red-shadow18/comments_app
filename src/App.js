71665751e65bb9053fcb29b65ee4d2c8a1b0432e
red-shadow18 <2018056@iiitdmj.ac.in.com>
2022-07-09 23:57:23 +0530

    Comment app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,158 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import CommentsForm from "./components/ComentsForm";
+import Comment from "./components/Comment";
+import { useEffect, useState } from "react";
+import { Button } from "@mui/material";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 
 function App() {
+  const [sortNewestFirst, setSortNewestFirst] = useState(true);
+
+  const [activeComment, setActiveComment] = useState(null);
+  const [comments, setComments] = useState(
+    JSON.parse(localStorage.getItem("commentsALL")) || []
+  );
+
+  const [rootComments, setRootComments] = useState([]);
+
+  useEffect(() => {
+    let sortedComments = [...comments];
+    if (comments.length > 0) {
+      if (!sortNewestFirst) {
+        sortedComments.sort((a, b) => a.id - b.id);
+      } else {
+        sortedComments.sort((b, a) => a.id - b.id);
+      }
+    }
+    setComments(sortedComments);
+    setRootComments(
+      sortedComments.filter((comment) => comment.parentId === null)
+    );
+  }, [comments, sortNewestFirst]);
+
+  const getReplies = (parentId) => {
+    return comments.filter((comment) => comment.parentId === parentId);
+  };
+
+  const updateComments = (comment, name, parentId, ownID) => {
+    const updatedComments = comments.map((newComment) => {
+      if (newComment.id === ownID) {
+        return {
+          ...newComment,
+          comment: comment,
+        };
+      }
+      return newComment;
+    });
+    setComments(updatedComments);
+    setActiveComment({});
+    window.localStorage.setItem("commentsALL", JSON.stringify(updatedComments));
+  };
+
+  const addComment = (comment, name, parentId = null) => {
+    let newComment = {
+      id: Date.now(),
+      name: name,
+      time: new Date(),
+      comment: comment,
+      parentId: parentId,
+    };
+
+    window.localStorage.setItem(
+      "commentsALL",
+      JSON.stringify([...comments, newComment])
+    );
+    setActiveComment({});
+    setComments([...comments, newComment]);
+  };
+  const [deletionID, setDeletionID] = useState(null);
+
+  useEffect(() => {
+    if (deletionID > 0) {
+      if (window.confirm("Are you sure you want to delete this comment?")) {
+        window.localStorage.setItem(
+          "commentsALL",
+          JSON.stringify(
+            comments.filter((comment) => comment.id !== deletionID)
+          )
+        );
+        setComments(comments.filter((comment) => comment.id !== deletionID));
+      }
+    }
+  }, [deletionID]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        flexDirection: "column",
+        alignItems: "center",
+        // width: "750px",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          //width: "750px",
+          // justifyContent: "flex-end",
+          paddingLeft: "25px",
+        }}
+      >
+        <CommentsForm formType="comment" onSubmit={addComment} />
+      </div>
+
+      {rootComments.length > 1 ? (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            width: "823px",
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Sort By : Date and Time{" "}
+          <b> {sortNewestFirst ? "(Newest First)" : "(Oldest First)"}</b>
+          <Button onClick={() => setSortNewestFirst(!sortNewestFirst)}>
+            {sortNewestFirst ? (
+              <ArrowUpwardIcon style={{ color: "black" }} />
+            ) : (
+              <ArrowDownwardIcon style={{ color: "black" }} />
+            )}
+          </Button>
+        </div>
+      ) : (
+        ""
+      )}
+
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          flexDirection: "column",
+          padding: "10px",
+        }}
+      >
+        {rootComments.map((comment) => {
+          return (
+            <Comment
+              key={comment.id}
+              name={comment.name}
+              time={comment.time}
+              id={comment.id}
+              comment={comment.comment}
+              parentId={comment.parentId}
+              replies={getReplies(comment.id)}
+              setDeletionID={setDeletionID}
+              activeComment={activeComment}
+              setActiveComment={setActiveComment}
+              addComment={addComment}
+              updateComments={updateComments}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
